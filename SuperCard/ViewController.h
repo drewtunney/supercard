985197aa5ae9f271afb66e94a0fa43060c156a3f
drewtunney <bryantunney@gmail.com>
2014-04-24 09:52:08 -0400

    first commit

---
+++ SuperCard/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SuperCard
+//
+//  Created by Drew Tunney on 4/24/14.
+//  Copyright (c) 2014 drewtunney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SuperCard/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  SuperCard
+//
+//  Created by Drew Tunney on 4/24/14.
+//  Copyright (c) 2014 drewtunney. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
